a8175898d800ae5b8984619aca707f2319b7c4ed
seej000 <seao9385@colorado.edu>
2024-03-22 09:58:16 +0000

    Fixed database connection

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World from --Seiji Aoyama-- in 3308'
 
-@app.route()
-def testing('/db_test'):
+@app.route('/db_test')
+def testing():
     conn = psycopg2.connect("postgres://lab10_db_ofde_user:vW4uKLc4YKccVCmclWRbrSMDic72bkZt@dpg-cnul33fjbltc73c4fpmg-a/lab10_db_ofde")
     conn.close()
     return "Database Connection Successful"
