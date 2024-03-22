9564928a5ca640d024a424063e93ce6e77ca76a7
seej000 <seao9385@colorado.edu>
2024-03-22 10:30:35 +0000

    Database creation

--- app.py
+++ app.py
@@ -11,3 +11,20 @@ def testing():
     conn = psycopg2.connect("postgres://lab10_db_ofde_user:vW4uKLc4YKccVCmclWRbrSMDic72bkZt@dpg-cnul33fjbltc73c4fpmg-a/lab10_db_ofde")
     conn.close()
     return "Database Connection Successful"
+
+@app.route('/db_create')
+def creating():
+    conn = psycopg2.connect("postgres://lab10_db_ofde_user:vW4uKLc4YKccVCmclWRbrSMDic72bkZt@dpg-cnul33fjbltc73c4fpmg-a/lab10_db_ofde")
+    cur = conn.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+''')
+    conn.commit()
+    conn.close()
+    return "Baseball Table Successfully Created"
